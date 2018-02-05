addf3aad49b82896ef5d03f80eb7843445da3a75
Ethan Cunningham <ethancunninghamcs@gmail.com>
2018-02-05 00:47:20 -0600

    Added a lot... Stopped mid function on showHistory. Tired, going to bed

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 /*
   ISH: Incredibly Simple Shell
 
-  Author:
+  Author: Ethan Cunningham
   MAT 4970
 */
 
@@ -10,49 +10,131 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 #define MAX_LINE_LENGTH 80               /* upper bounds on line */
 #define MAX_ARGUMENTS MAX_LINE_LENGTH/2  /* and number of arguments */
 #define DELIMITER " "                    /* Used to tokenize lines */
-
+#define MAX_COMMANDS 10                  /* Used to determine how far the user can look back at commands */
 
 typedef int bool;
 #define FALSE 0
 #define TRUE 1
 
+
+typedef struct {
+    char commandHistory[MAX_COMMANDS][MAX_LINE_LENGTH + 1];
+    int commandNumber;
+} history;
+
+
+void logArgument(history *hist, int commandNumber, char *args[]);
 void readAndParseArgs(char inputBuffer[], char *args[]);
 void displayArgs(char *args[]);
 int num_words_in_sent(char *sentence);
-
-
+bool exitCommand(char *args);
+bool historyCommand(char *args);
+bool exclamationCommand(char *args);
+void showHistory(history *hist);
 
 
 int main(void) {
     char inputBuffer[MAX_LINE_LENGTH + 1];   /* buffer to hold the command entered */
     char *args[MAX_ARGUMENTS + 1];           /* array of arguments */
-    pid_t pid;
+    pid_t pid;                               /* pid for processes */
+    int wstatus;                             /* status code to be used for wait */
+    int commandIndex = 1;                    /* Keeps track of what command you are on */
+    history ish_history;
+
+
 
     while (TRUE) {
         /* Provide a prompt for the command line */
-        printf("ISH> ");
+        printf("%3d ISH> ", commandIndex);
         fflush(stdout);   /* force output to appear */
+        commandIndex++;
 
         /* Get a command line */
         readAndParseArgs(inputBuffer, args);
 
-        /* Show the individual arguments */
-        displayArgs(args);
 
-        /* Create a child process, execute the specified command,
-           and wait for completion */
-        
+        /* Checks if there was no input. Prevents a segfault */
+        if (num_words_in_sent(inputBuffer) > 0) {
+
+            /* Show the individual arguments */
+//            displayArgs(args);
+
+            /* checks if command was internal */
+            if (exitCommand(args[0]) == TRUE) {
+                return 0;
+            } else if (historyCommand(args[0]) == TRUE) {
+                logArgument(&ish_history, commandIndex, args);
+                printf("place history function here\n");
+            } else if (exclamationCommand(args[0]) == TRUE) {
+                logArgument(&ish_history, commandIndex, args);
+                printf("Insert command history exclamation function here.\n");
+            } else {
+
+                /* Logging the argument */
+                logArgument(&ish_history, commandIndex, args);
+
+                /* Create a child process, execute the specified command,
+                   and wait for completion */
+
+                if ((pid = fork()) < 0) {
+                    fprintf(stderr, "Process could not fork.\n");
+                } else if (pid == 0) {
+                    if (execvp(*args, args) < 0) {
+                        fprintf(stderr, "ERROR in execvp call\n Make sure the input commands were correct\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    exit(EXIT_SUCCESS);
+                } else {
+                    if (wait(&wstatus) < 0) {
+                        fprintf(stderr, "ERROR occurred while the parent was waiting for child\n");
+                    }
+                }
+            }
+        }
     }
 }
 
 
+void showHistory(history *hist) {
+    int index = hist->commandNumber;
+    int tempComNum = hist->commandNumber;
+    if (index > 10) {
+        index++;
+        for (int i)
+    }
+}
 
 
+bool exclamationCommand(char *args) {
+    return *args == '!' ? TRUE : FALSE;
+}
+
+
+
+void logArgument(history *hist, int commandNumber, char *args[]) {
+    int index;
+    for (index = 0; args[index] != NULL; index++) {
+        hist->commandHistory[commandNumber % 10][index] = args[index];
+    }
+    hist->commandNumber = commandNumber;
+}
+
+
+bool historyCommand(char *args) {
+    return strcmp(args, "history") == 0 ? TRUE : FALSE;
+}
+
+
+bool exitCommand(char *args) {
+    return strcmp(args, "exit") == 0 ? TRUE : FALSE;
+}
 
 
 /* Used to count the number of words in a line. */
@@ -97,9 +179,11 @@ void readAndParseArgs(char inputBuffer[], char *args[]) {
     /* Transform the inputBuffer to a C-style string */
     inputBuffer[length - 1] = '\0';
 
-    printf("Line entered = \"%s\"\n", inputBuffer);
-
-
+//    printf("Line entered = \"%s\"\n", inputBuffer);
+    int numWords = num_words_in_sent(inputBuffer);
+    if (numWords == 0) {
+        return;
+    }
 
     /* Isolate the arguments found on the input line and
        store them in the argument vector */
@@ -115,6 +199,8 @@ void readAndParseArgs(char inputBuffer[], char *args[]) {
     }
     args[index] = NULL;
 }
+
+
 
 void displayArgs(char *args[]) {
     /* Output the arguments in a given argument vector */
