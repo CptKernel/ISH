337c498bc66edb70f0a6ac4cbd8097ae17df9133
Ethan Cunningham <ethancunninghamcs@gmail.com>
2018-02-12 19:51:06 -0600

    Implemented the & and ! functionality, and made it much harder to make Dynamic.

--- main.c
+++ main.c
@@ -16,7 +16,8 @@
 #define MAX_LINE_LENGTH 80               /* upper bounds on line */
 #define MAX_ARGUMENTS (MAX_LINE_LENGTH/2)  /* and number of arguments */
 #define DELIMITER " "                    /* Used to tokenize lines */
-#define MAX_COMMANDS 10                  /* Used to determine how far the user can look back at commands */
+#define MAX_COMMANDS_START  10
+int MAX_COMMANDS = 10;                  /* Used to determine how far the user can look back at commands */
 
 typedef int bool;
 #define FALSE 0
@@ -24,7 +25,7 @@ typedef int bool;
 
 
 typedef struct history {
-    char commandHistory[MAX_COMMANDS][MAX_LINE_LENGTH + 1];
+    char commandHistory[MAX_COMMANDS_START][MAX_LINE_LENGTH + 1];
     int commandNumber;
 } history;
 
@@ -47,9 +48,9 @@ int main(void) {
     int wstatus;                             /* status code to be used for wait */
     int commandIndex = 0;                    /* Keeps track of what command you are on */
     history ish_history;
-    int zomCount = 0;
+    char currentWorkingDir[2048];
 
-
+    MAX_COMMANDS = MAX_COMMANDS * 2;
 
     while (TRUE) {
         /* Provide a prompt for the command line */
@@ -71,6 +72,7 @@ int main(void) {
             args[pIndex - 1] = NULL;
         }
 
+
         /* Checks if there was no input. Prevents a segfault */
         if (num_words_in_sent(inputBuffer) > 0) {
 
@@ -88,16 +90,23 @@ int main(void) {
                     }
                     exit(EXIT_SUCCESS);
                 } else {
-                    printf("[%d] %d\n", ++zomCount, pid);
+                    printf("%d\n", pid);
+                    waitpid(pid, &wstatus, WNOHANG);
                     fflush(stdout);
                 }
             } else if (historyCommand(args[0]) == TRUE) {
                 showHistory(&ish_history);
                 logArgument(&ish_history, commandIndex, args);
             } else if (exclamationCommand(args[0]) == TRUE) {
-//                executeHistoryCommand(args, ish_history);
+                executeHistoryCommand(args, &ish_history);
                 logArgument(&ish_history, commandIndex, args);
-                printf("Insert command history exclamation function here.\n");
+            } else if (strcmp(args[0], "cd") ==  0) {
+                logArgument(&ish_history, commandIndex, args);
+                chdir(args[1]);
+            } else if (strcmp(args[0], "pwd") == 0) {
+                logArgument(&ish_history, commandIndex, args);
+                getcwd(currentWorkingDir, sizeof(currentWorkingDir));
+                printf("%s\n", currentWorkingDir);
             } else {
 
                 /* Logging the argument */
@@ -138,7 +147,108 @@ void parseArgs(history *hist, char *args[], int commandToGet) {
 }
 
 
+void executeHistoryCommand(char *args[],  history *hist) {
+    pid_t pid;
+    char currentWorkingDir[2048];
+    int wstatus;
+    int historyNumber = 0;
+    char *exclamationString = args[0];
+    int argsLength = (int) strlen(exclamationString);
+    char number[argsLength];
+    for (int i = 1; i < argsLength; i++) {
+        number[i - 1] = exclamationString[i];
+    }
+    number[argsLength - 1] = '\0';
+    historyNumber = atoi(number);
+    historyNumber = historyNumber % MAX_COMMANDS;
+    int tempIndex = 0;
+    int argsIndex = 0;
+    char inputBuffer[MAX_LINE_LENGTH + 1];
+    while (hist->commandHistory[historyNumber][tempIndex] != '\0') {
+        inputBuffer[tempIndex] = hist->commandHistory[historyNumber][tempIndex];
+        tempIndex++;
+    }
+    inputBuffer[tempIndex] = '\0';
 
+    int numWordsEHC = num_words_in_sent(inputBuffer);
+    if (numWordsEHC == 0) {
+        return;
+    }
+
+    /* Isolate the arguments found on the input line and
+       store them in the argument vector */
+    char *ptr;
+    ptr = strtok(inputBuffer, DELIMITER);
+    int index = 0;
+
+    while (ptr != NULL) {
+        args[index] = ptr;
+
+        index++;
+        ptr = strtok(NULL, DELIMITER);
+    }
+    args[index] = NULL;
+
+    /* This is grabbing the last arg to see if it is an & */
+    int pIndex;
+    char *lastArg = "";
+    for (pIndex = 0; args[pIndex] != NULL; pIndex++) {
+        lastArg = args[pIndex];
+    }
+
+    /* If it is an &, No longer want it being read as an instruction here */
+    if (strcmp(lastArg, "&") == 0) {
+        args[pIndex - 1] = NULL;
+    }
+
+
+    /* Checks if there was no input. Prevents a segfault */
+    if (num_words_in_sent(inputBuffer) > 0) {
+
+        /* checks if command was internal */
+        if (strcmp(lastArg, "&") == 0) {
+            if ((pid = fork()) < 0) {
+                fprintf(stderr, "Process could not fork.\n");
+            } else if (pid == 0) {
+                if (execvp(*args, args) < 0) {
+                    fprintf(stderr, "ERROR in execvp call\n Make sure the input commands were correct\n");
+                    exit(EXIT_FAILURE);
+                }
+                exit(EXIT_SUCCESS);
+            } else {
+                printf("%d\n", pid);
+                waitpid(pid, &wstatus, WNOHANG);
+                fflush(stdout);
+            }
+        } else if (historyCommand(args[0]) == TRUE) {
+            showHistory(hist);
+        } else if (strcmp(args[0], "cd") ==  0) {
+            chdir(args[1]);
+            printf("\n");
+        } else if (strcmp(args[0], "pwd") == 0) {
+            getcwd(currentWorkingDir, sizeof(currentWorkingDir));
+            printf("%s\n", currentWorkingDir);
+        } else {
+
+            /* Create a child process, execute the specified command,
+               and wait for completion */
+
+            if ((pid = fork()) < 0) {
+                fprintf(stderr, "Process could not fork.\n");
+            } else if (pid == 0) {
+                if (execvp(*args, args) < 0) {
+                    fprintf(stderr, "ERROR in execvp call\n Make sure the input commands were correct\n");
+                    exit(EXIT_FAILURE);
+                }
+                exit(EXIT_SUCCESS);
+            } else {
+                if (wait(&wstatus) < 0) {
+                    fprintf(stderr, "ERROR occurred while the parent was waiting for child\n");
+                }
+            }
+        }
+    }
+}
 
 
 
@@ -146,19 +256,19 @@ void showHistory(history *hist) {
     printf("\n");
     int tempCommandNumber = hist->commandNumber;
     int displayCommandNumber;
-    if (tempCommandNumber > 10) {
+    if (tempCommandNumber >= MAX_COMMANDS) {
         displayCommandNumber = hist->commandNumber - 9;
         tempCommandNumber++;
         while ((tempCommandNumber %  MAX_COMMANDS) != (hist->commandNumber % MAX_COMMANDS)) {
             printf("%3d ", displayCommandNumber++);
-            for (int i = 0; hist->commandHistory[tempCommandNumber % MAX_COMMANDS][i] != NULL; i++) {
+            for (int i = 0; hist->commandHistory[tempCommandNumber % MAX_COMMANDS][i] != '\0'; i++) {
                 printf("%c", hist->commandHistory[tempCommandNumber % MAX_COMMANDS][i]);
             }
             tempCommandNumber++;
             printf("\n");
         }
         printf("%3d ", displayCommandNumber);
-        for (int i = 0; hist->commandHistory[tempCommandNumber % MAX_COMMANDS][i] != NULL; i++) {
+        for (int i = 0; hist->commandHistory[tempCommandNumber % MAX_COMMANDS][i] != '\0'; i++) {
             printf("%c", hist->commandHistory[tempCommandNumber % MAX_COMMANDS][i]);
         }
         printf("\n");
@@ -166,7 +276,7 @@ void showHistory(history *hist) {
         displayCommandNumber = 0;
         while (displayCommandNumber <= hist->commandNumber) {
             printf("%3d ", displayCommandNumber);
-            for (int i = 0; hist->commandHistory[displayCommandNumber][i] != NULL; i++) {
+            for (int i = 0; hist->commandHistory[displayCommandNumber][i] != '\0'; i++) {
                 printf("%c", hist->commandHistory[displayCommandNumber][i]);
             }
             displayCommandNumber++;
@@ -190,13 +300,13 @@ void logArgument(history *hist, int commandNumber, char *args[]) {
     for (argsIndex = 0; args[argsIndex] != NULL; argsIndex++) {
         char *temp = args[argsIndex];
         int charIndex = 0;
-        for (histIndex; temp[charIndex] != NULL; histIndex++) {
+        for (histIndex; temp[charIndex] != '\0'; histIndex++) {
             hist->commandHistory[commandNumber % MAX_COMMANDS][histIndex] = temp[charIndex++];
         }
         hist->commandHistory[commandNumber % MAX_COMMANDS][histIndex++] = ' ';
     }
     hist->commandNumber = commandNumber;
-    hist->commandHistory[commandNumber % MAX_COMMANDS][histIndex - 1] = NULL;
+    hist->commandHistory[commandNumber % MAX_COMMANDS][histIndex - 1] = '\0';
 
 }
 
@@ -254,8 +364,8 @@ void readAndParseArgs(char inputBuffer[], char *args[]) {
     inputBuffer[length - 1] = '\0';
 
 //    printf("Line entered = \"%s\"\n", inputBuffer);
-    int numWords = num_words_in_sent(inputBuffer);
-    if (numWords == 0) {
+    int numWordsRAPA = num_words_in_sent(inputBuffer);
+    if (numWordsRAPA == 0) {
         return;
     }
 
